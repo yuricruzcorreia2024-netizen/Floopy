179f41893bcd24e4db559d694d9c19f1fcf977fb
DKADDONS <yuricruzcorreia2024@gmail.com>
2026-09-05 15:51:22 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,5 +2,5 @@
 // Em desenvolvimento local, use http://localhost:1010
 // Quando publicar sua SugoiAPI, troque pela URL HTTPS pública.
 window.FLOPPY_CONFIG = {
-    API_URL: "http://localhost:1010"
+    API_URL: "https://animefire.io/"
 };
